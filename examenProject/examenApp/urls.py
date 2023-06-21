3af8e3bf1ef87948538ae27fa778a38ca6d1610b
gdomaica <guillermo.domaica@opendeusto.es>
2023-06-21 20:43:57 +0200

    modificaciones

--- examenProject/examenApp/urls.py
+++ examenProject/examenApp/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
  path('examen/', views.oficinaListView.as_view(), name='oficina'),
  path('examen/<int:pk>', views.oficninaDetailView.as_view(), name='detalle'),
  path('formulario/', views.oficinaCreateView.as_view(), name='formulario'),
-path('formularioEmpleado/', views.empleadoCreateView.as_view(), name='formularioEmpleado')
+ path('formularioEmpleado/', views.empleadoCreateView.as_view(), name='formularioEmpleado')
 ]
--- examenProject/examenApp/views.py
+++ examenProject/examenApp/views.py
@@ -8,22 +8,22 @@ from .models import oficina, empleado
 
 class oficinaListView(ListView):
     model = oficina
-    template_name = 'oficina.html'
+    template_name = 'oficina_list.html'
     queryset = oficina.objects.order_by('ciudad')
 
 class oficninaDetailView(DetailView):
     model = oficina
-    template_name = 'detalle.html'
+    template_name = 'oficina_detail.html'
 
 class oficinaCreateView(CreateView):
     model = oficina
     fields = '__all__'
-    template_name = 'formulario.html'
+    template_name = 'oficina_create.html'
     success_url = 'http://127.0.0.1:8000/examen/'
 
 class empleadoCreateView(CreateView):
     model = empleado
     fields = '__all__'
-    template_name = 'formularioEmpleado.html'
+    template_name = 'empleado_create.html'
     success_url = 'http://127.0.0.1:8000/examen/<int:pk>'
 
